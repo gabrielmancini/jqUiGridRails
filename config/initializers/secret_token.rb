b7f446e053ac857b8dfcbd8b1e1fcbdcd29e413e
gabrielmancini <gabriel.mancini@gmail.com>
2010-10-15 11:53:16 -0300

    new file:   Gemfile 	new file:   Rakefile 	new file:   app/controllers/application_controller.rb 	new file:   app/helpers/application_helper.rb 	new file:   app/views/layouts/application.html.erb 	new file:   config.ru 	new file:   config/application.rb 	new file:   config/boot.rb 	new file:   config/database.yml 	new file:   config/environment.rb 	new file:   config/environments/development.rb 	new file:   config/environments/production.rb 	new file:   config/environments/test.rb 	new file:   config/initializers/backtrace_silencers.rb 	new file:   config/initializers/inflections.rb 	new file:   config/initializers/mime_types.rb 	new file:   config/initializers/secret_token.rb 	new file:   config/initializers/session_store.rb 	new file:   config/locales/en.yml 	new file:   config/routes.rb 	new file:   db/seeds.rb 	new file:   doc/README_FOR_APP 	new file:   lib/tasks/.gitkeep 	new file:   public/404.html 	new file:   public/422.html 	new file:   public/500.html 	new file:   public/favicon.ico 	new file:   public/images/rails.png 	new file:   public/index.html 	new file:   public/javascripts/application.js 	new file:   public/javascripts/controls.js 	new file:   public/javascripts/dragdrop.js 	new file:   public/javascripts/effects.js 	new file:   public/javascripts/prototype.js 	new file:   public/javascripts/rails.js 	new file:   public/robots.txt 	new file:   public/stylesheets/.gitkeep 	new file:   script/rails 	new file:   test/performance/browsing_test.rb 	new file:   test/test_helper.rb 	new file:   vendor/plugins/.gitkeep

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JqUiGridRails::Application.config.secret_token = '44fd8410e078519e47da306203f2244414050880e212aea6ee593391c4c2ddeb00de859547e31ec785dcc0eca23d4645eca7bbbdfaab2dc5acc25ff6ecc039a7'
